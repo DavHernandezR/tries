32bdf2305d5cb730cd0497aab1c4f8c6abd0f54d
David Andres Hernandez Rodriguez <DavidAndres.HernandezRodriguez@endava.com>
2022-04-04 11:39:34 -0500

    Cambios

---
+++ src/main/java/base/MyMap.java
@@ -0,0 +1,8 @@
+package base;
+
+public class MyMap {
+
+    public static void main(String[] args){
+
+    }
+}
---
+++ src/main/java/base/MyString.java
@@ -0,0 +1,65 @@
+package base;
+
+public class MyString {
+
+    static String reversarFrase(String phrase) {
+        String[] palabras = phrase.split(" ");
+        String nuevaPhrase = palabras[palabras.length - 1];
+        for (int i = 1; i <= palabras.length - 1; i++) {
+            nuevaPhrase = nuevaPhrase + " " + palabras[palabras.length - i - 1];
+        }
+        System.out.println("La cantidad de palabras que contiene la frase es: " + palabras.length);
+        return nuevaPhrase;
+    }
+
+    static String reversarPalabras(String phrase){
+
+        char[] letras = new char[phrase.length()];
+        phrase.getChars(0,phrase.length(), letras, 0);
+        String nuevaPhrase = String.valueOf(letras[letras.length-1]);
+
+        for(int i = 2; i <= letras.length; i++){
+            nuevaPhrase = nuevaPhrase + letras[letras.length-i];
+        }
+
+        return nuevaPhrase;
+    }
+
+    static Integer contarLetras(String phrase){
+        int contador = 0;
+        int longitud = phrase.length();
+        String espacio = " ";
+        char spacio = espacio.charAt(0);
+        for (int i = 0; i <= longitud-1; i++) {
+            char caracter = phrase.charAt(i);
+            if (caracter == spacio) {
+                contador++;
+            }
+        }
+        System.out.println("La cantidad de letras es: " + (longitud - contador));
+        System.out.println("La cantidad de espacios es: " + contador);
+        return 0;
+    }
+
+    static String encontrarPalabra(String phrase, String palabraBuscada){
+        int posicionPalabra = phrase.indexOf(palabraBuscada);
+        if ( posicionPalabra == -1) {
+            System.out.println("La palabra buscada no existe");
+        }
+        else{
+            System.out.println("La palabra buscada es la " + posicionPalabra + " palabra");
+
+        }
+        return "";
+    }
+
+    public static void main(String[] args){
+        String phrase = "I am learning about classes and strings and maps";
+        String palabraBuscada = "and";
+        System.out.println(reversarFrase(phrase));
+        System.out.println(reversarPalabras(phrase));
+
+        contarLetras(phrase);
+        encontrarPalabra(phrase, palabraBuscada);
+    }
+}
